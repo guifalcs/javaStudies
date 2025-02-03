afa5f0c478e208fb1334931bf4cc2f0c3e80dcbd
Guilherme <guifalcao2017@gmail.com>
2025-02-03 10:14:03 -0300

    LinkedLists and Songs

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
+        Album caminho = new Album("Caminho", "Psalms");
+        caminho.addSong("Caminho", 3.5);
+        caminho.addSong("Três Dias", 4.0);
 
-        MobilePhone meuCellphone = new MobilePhone("1234567890");
-        Contact poatan = Contact.createContact("Poatan", "0987654321");
-        Contact alex = Contact.createContact("Alex Poatan", "0987654321");
-
-
-        meuCellphone.addNewContact(Contact.createContact(poatan.getName(), poatan.getPhoneNumber()));
-        meuCellphone.addNewContact(Contact.createContact(alex.getName(), alex.getPhoneNumber()));
-        meuCellphone.printContacts();
-        System.out.println("Vou remover o alex");
-        meuCellphone.removeContact(alex);
-        meuCellphone.printContacts();
-
+        System.out.println(caminho.findSong("Caminho"));
     }
 
 }
---
+++ Song.java
@@ -0,0 +1,18 @@
+public class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
