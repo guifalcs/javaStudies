49b3a20f17c78706a6dbed2d1df8eff93dc153da
Guilherme <guifalcao2017@gmail.com>
2025-02-04 09:44:53 -0300

    Interfaces and Monsters

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
-        Album caminho = new Album("Caminho", "Psalms");
-        caminho.addSong("Caminho", 3.5);
-        caminho.addSong("Três Dias", 4.0);
-
-        System.out.println(caminho.findSong("Caminho"));
     }
 
 }
