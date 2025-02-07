654cd7d9742d287b79c61e05e000b87948a53a21
Guilherme <guifalcao2017@gmail.com>
2025-02-07 13:30:41 -0300

    seeing records

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
+        record Person( String firstname, String surname){
+            @Override
+            public String toString() {
+                return firstname + ' ' + surname;
+            }
+        };
+        Person p = new Person("John", "Doe");
+        System.out.println(p.toString());
     }
 
 }
